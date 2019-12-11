99bb61e3d9869c842d3af64c36e2e4d4f540a2a0
Tucker Kent <tbkent1@mail.mccneb.edu>
2019-12-11 17:29:13 -0600

    Create kent_a02b.js

---
+++ kent_a02b.js
@@ -0,0 +1,21 @@
+/*Tucker Kent
+  kent_a02b.js
+  19WI_INFO_2124_WW
+  Thoendel_
+  11 December 2019
+*/
+
+let fullName = "Tucker Kent";  
+var age = 29;
+const hairColor = "Brown";
+const eyeColor = "Brown";
+
+console.log();
+console.log("My name:");
+console.log(fullName);
+console.log("My age:");
+console.log(age);
+console.log("My hair color:");
+console.log(hairColor);
+console.log("My eye color:");
+console.log(eyeColor);
