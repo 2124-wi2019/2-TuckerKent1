60bb9d257c70d3cfe2dc3dff87848f4f032ee112
Tucker Kent <tbkent1@mail.mccneb.edu>
2019-12-18 18:10:42 -0600

    Assignment 3c JavaScript

---
+++ kent_a03c.js
@@ -0,0 +1,19 @@
+/*
+Tucker Kent
+kent_a03c.js
+19WI_INFO_2124_WW
+Thoendel
+Created 18 December 2019
+*/
+
+let preambleString = "When in the course of human events"; // variable declared and initialized with a string value
+
+let preambleLiteral = `
+The preamble is:
+\t${preambleString}
+
+Does the preamble contain 'a'?
+\t${preambleString.includes("a")}
+`; // placed the template literal in variable preambleLiteral for ease of logging it
+
+console.log(preambleLiteral); // output text of preambleLiteral to terminal
